34b5fc1e7e67997e8a885b719a9431c30597bdd5
marcvifi10 <36198307+marcvifi10@users.noreply.github.com>
2019-09-21 13:25:00 +0200

    c#

---
+++ 57 - Struct/Empleados/ConsoleApplication1/Empleado.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    struct Empleado
+    {
+
+        public double salarioBase, comision;
+
+        public Empleado(int salarioBase, int comision)
+        {
+
+            this.salarioBase = salarioBase;
+
+            this.comision = comision;
+
+        }
+
+        public override string ToString()
+        {
+
+            return string.Format("Salario y comisión del empleado ({0},{1})", this.salarioBase, this.comision);
+
+        }
+
+        public void cambiaSalario(Empleado emp, double incremento)
+        {
+
+            emp.salarioBase += incremento;
+
+            emp.comision += incremento;
+
+        }
+
+    }
+}
---
+++ 57 - Struct/Empleados/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Empleado e1 = new Empleado(1200, 250);
+
+            e1.cambiaSalario(e1, 200);
+
+            Console.WriteLine(e1);
+
+        }
+    }
+}
